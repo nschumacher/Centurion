bd8bb0e057a3dcb6e81e72be45fbfb4466f5c40d
Nick Schumacher <nschuma@purdue.edu>
2017-02-06 01:33:45 -0500

    Various
    - changed up some colors
    - corrected image tags
    - partially styled the attack show page

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,3 +10,8 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 Rails.application.config.assets.precompile += %w( navbar.scss )
+Rails.application.config.assets.precompile += %w( attacks.scss )
+Rails.application.config.assets.precompile += %w( pages.scss )
+Rails.application.config.assets.precompile += %w( sidebar_nav.scss )
+Rails.application.config.assets.precompile += %w( centurion_1.png )
+Rails.application.config.assets.precompile += %w( green_check.png )
